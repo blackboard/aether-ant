5d8938de670cbdbbe8a267c8bf09c031cc8f8b97
Danny Thomas <dthomas@blackboard.com>
2014-01-13 11:48:41 +0000

    Minor updates to better reflect master

--- src/test/java/org/eclipse/aether/ant/DeployTest.java
+++ src/test/java/org/eclipse/aether/ant/DeployTest.java
@@ -16,8 +16,6 @@ import static org.hamcrest.Matchers.*;
 import java.io.File;
 import java.util.Arrays;
 
-import org.apache.tools.ant.Project;
-import org.eclipse.aether.internal.test.util.TestFileUtils;
 /*
  * still missing:
  * - deploy snapshots/releases into correct repos
@@ -25,28 +23,6 @@ import org.eclipse.aether.internal.test.util.TestFileUtils;
 public class DeployTest
     extends AntBuildsTest
 {
-    private File distRepoPath;
-
-    @Override
-    protected void setUp()
-        throws Exception
-    {
-        super.setUp();
-        distRepoPath = new File( "target/dist-repo" );
-        System.setProperty( "project.distrepo.url", distRepoPath.toURI().toString() );
-        TestFileUtils.delete( distRepoPath );
-
-        configureProject( "src/test/ant/Deploy.xml", Project.MSG_VERBOSE );
-    }
-
-    @Override
-    protected void tearDown()
-        throws Exception
-    {
-        super.tearDown();
-        TestFileUtils.delete( distRepoPath );
-    }
-
     public void testDeployGlobalPom()
     {
         executeTarget( "testDeployGlobalPom" );
